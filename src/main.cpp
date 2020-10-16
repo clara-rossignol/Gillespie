68b2aeba9281bfce319a40716266138782d5e13c
jrougemo <jacques.rougemont@epfl.ch>
2020-10-16 09:01:14 +0200

    main.cpp added

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include "constants.h"
+#include "Random.h"
+#include "Simulation.h"
+
+
+RandomNumbers *_RNG;
+
+int main(int argc, char **argv) {    
+    try {
+        Simulation s(argc, argv);
+        s.run();
+    } catch(SimulError &e) {
+        std::cerr << e.what() << std::endl;
+        return e.value();
+    }
+    if (_RNG) delete _RNG;
+    return 0;
+}
