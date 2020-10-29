e74fb64ccb7f947378ff3bf540a9240c8515a315
Rossignol Clara <rossigno@INTRANET.EPFL.CH@SVUbuUniv6.intranet.epfl.ch>
2020-10-29 16:09:31 +0100

    few changes in Random.cpp

--- src/Random.cpp
+++ src/Random.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "Random.h"
 
 RandomNumbers::RandomNumbers(unsigned long int s) : seed(s) {
@@ -19,12 +20,6 @@ int RandomNumbers::uniform_int(int lower, int upper)
 	return unif(rng);
 }
 
-template<class T> void RandomNumbers::uniform_int(T &res, int lower, int upper)
-{
-	std::uniform_int_distribution<> unif(lower, upper);
-	for (auto I=res.begin(); I!=res.end(); I++) *I = unif(rng);
-}
-
 double RandomNumbers::normal(double mean, double sd)
 {
 	std::normal_distribution<> norm(mean, sd);
@@ -33,24 +28,12 @@ double RandomNumbers::normal(double mean, double sd)
 
 double RandomNumbers::exponential(const double rate)
 {
-	std::exponential_distribution <> expo(rate);
-	return expo(rng);
-}
-
-template<class T> void RandomNumbers::exponential(T &res, const double rate)
-{
 	std::exponential_distribution<> expo(rate);
-	for (auto I=res.begin(); I!=res.end(); I++) *I = expo(rng);
+	return expo(rng);
 }
 
 int RandomNumbers::poisson(double mean)
 {
 	std::poisson_distribution<> poiss(mean);
 	return poiss(rng);
-}
-
-template<class T> void RandomNumbers::poisson(T &res, double mean)
-{
-	std::poisson_distribution<> poiss(mean);
-	for (auto I=res.begin(); I!=res.end(); I++) *I = poiss(mean);
 }
